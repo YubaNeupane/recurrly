53e0a662672d99a9110648321bf4f1f08606fb51
YubaNeupane <neopaneyb2@gmail.com>
2026-04-05 15:59:56 -0400

    bug fix?

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,7 +1,7 @@
-import { useFonts } from "expo-font";
 import { SplashScreen, Stack } from "expo-router";
 import { useEffect } from "react";
 import "../global.css";
+SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
   const fontsLoaded = useFonts({
